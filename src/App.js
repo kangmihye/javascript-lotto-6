0ce51de21977c9c43d91ef64918b503d0f2dfde2
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-22 17:43:05 +0900

    feat: App에서 각각의 결과 값 확인
    - 결과값 확인

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import compareResult from "./domain/compareResult.js";
 import LottoList from "./model/LottoList.js";
 import InputView from "./view/InputView.js";
 import OutputView from "./view/OutputView.js";
+import InputViewService from "./view/viewService/InputViewService.js";
 
 class App {
 	async play() {
@@ -9,10 +11,18 @@ class App {
 		const lottoList = OutputView.printRandomLottos(OutputView.printBoughtLottos(inputPrice), new LottoList());
 		console.log(lottoList.get()); //[ Lotto {}, Lotto {} ]
 
-		const inputWinningNumber = await InputView.readWinningNumber();
-		console.log(inputWinningNumber);
-		const inputBonusNumber = await InputView.readBonusNumber();
-		console.log(inputBonusNumber);
+		const parsedWinningNumber = InputViewService.parseInput(await InputView.readWinningNumber());
+		console.log(parsedWinningNumber);
+
+		const resultsWinningNum = compareResult.getResultCompareWinningNum(lottoList, parsedWinningNumber);
+
+		const bonusNumbers = await InputView.readBonusNumber();
+
+		console.log("위닝결과", resultsWinningNum);
+
+		const resultsBonusNum = compareResult.getResultCompareBonusNum(lottoList, resultsWinningNum, bonusNumbers);
+
+		console.log("보너스 결과", resultsBonusNum);
 	}
 }
 
