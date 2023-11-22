b66248fa44015a5fe2b67dde7b0eae26eb3bc744
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-22 17:42:32 +0900

    feat: 보너스 번호와 비교한 결과 반환
    - 로또 리스트에서 보너스 번호가 일치하면 5.5로 바꾼 결과만 반환

--- src/domain/compareResult.js
+++ src/domain/compareResult.js
@@ -2,7 +2,9 @@ const compareResult = {
 	getResultCompareWinningNum: (lottoList, winningNumbers) => {
 		return lottoList.compareTotalWinningNum(winningNumbers);
 	},
-	
+	getResultCompareBonusNum: (lottoList, resultsWinningNum, bonusNumbers) => {
+		return lottoList.compareTotalBonusNum(resultsWinningNum, Number(bonusNumbers));
+	},
 };
 
 export default compareResult;
