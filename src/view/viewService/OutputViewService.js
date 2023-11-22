cd499ae56cf94f8d24b03496ce759b7c10815313
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-22 18:28:41 +0900

    feat: 소수점 둘째자리 반올림 구현
    - roundSecond

--- src/view/viewService/OutputViewService.js
+++ src/view/viewService/OutputViewService.js
@@ -1,5 +1,6 @@
 const OutputViewService = {
 	countInputLotto: (inputPrice) => Math.floor(inputPrice / 1000),
+	roundSecond: (rate) => Math.round(rate * 100) / 100,
 };
 
 export default OutputViewService;
