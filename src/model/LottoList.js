2bf5bafbaee0008dbc6b2197530ae8814e94cedf
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-22 17:39:54 +0900

    feat: 보너스 넘버가 포함되어있는지 비교하는 기능 구현
    - 로또 리스트에 보너스 넘버가 포함 되어있는지 확인
    compareTotalBonusNum

--- src/model/LottoList.js
+++ src/model/LottoList.js
@@ -18,8 +18,22 @@ class LottoList {
 		this.LottoList.forEach((lotto) => {
 			equalNumbers.push(lotto.compareWinningNum(winningNumbers));
 		});
-
 		return equalNumbers;
+	}
+
+	compareTotalBonusNum(resultsWinningNum, bonusNumbers) {
+		const equalFiveIdx = resultsWinningNum.reduce((idxArr, num, idx) => {
+			if (num === 5) idxArr.push(idx);
+			return idxArr;
+		}, []);
+
+		const equalBonusIdx = equalFiveIdx.filter((idx) => this.LottoList[idx].isIncludedBonusNum(bonusNumbers));
+
+		equalBonusIdx.forEach((idx) => {
+			resultsWinningNum[idx] = 5.5;
+		});
+
+		return resultsWinningNum;
 	}
 }
 
